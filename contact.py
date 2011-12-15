68747e818d62e66e4d9c3b06df753837c2dd7cd7
SPLAWSKI David <splawski.david@gmail.com>
2011-12-15 15:29:22 +0100

    Verify errors with flake8

--- contact.py
+++ contact.py
@@ -5,6 +5,7 @@ __version__ = "0.2"
 
 import time
 
+
 class Contact:
     def __init__(self, first_name="", last_name="", email=""):
         "Contact's constructor"
@@ -17,11 +18,10 @@ class Contact:
         timestamp = int(time.time())
         return "%d" % timestamp
 
-
     def show_contact(self, last_name, first_name, email):
         "Show contact's informations"
-        print "First name and last name:", self.first_name, self.last_name, "E-mail:", self.email
-
+        print "First name and last name:", self.first_name, self.last_name,
+        "E-mail:", self.email
 
     def get_contact_ldif(self, first_name, last_name, email):
         "Return contact'si informations (format LDIF)"
@@ -38,7 +38,6 @@ class Contact:
         info += "modifytimestamp: %s\n" % self.get_timestamp()
         return info
 
-
     def get_data(self):
         "Return the informations entered"
         first_name = raw_input("First name : ")
@@ -46,8 +45,7 @@ class Contact:
         email = raw_input("E-mail : ")
         return [first_name, last_name, email]
 
-
-    def write_in_file(self,filename):
+    def write_in_file(self, filename):
         "Write in a file"
         of = open(filename, 'a')
         while 1:
@@ -58,7 +56,6 @@ class Contact:
                 info = self.get_contact_ldif(first_name, last_name, email)
                 of.write(info)
         of.close()
-
 
     def read_in_file(self, filename):
         "Read in a file"
@@ -73,7 +70,7 @@ class Contact:
 
 if __name__ == '__main__':
     filename = raw_input('Enter filename to process: ')
-    option = raw_input('Type "a" to add contacts or "s" to show contact list: ')
+    option = raw_input('Type "a" to add contacts or "s" to show contact list:')
     contact = Contact()
     if option == 'a':
         contact.write_in_file(filename)
